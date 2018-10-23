2763ab5b7c5f344c30ef0de025ddd9437a458899
lie-sun <1466548001@qq.com>
2018-10-23 19:45:29 +0800

    push

---
+++ informationmanagementsystem/src/components/common/Header/Header.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import './Header.less'
+
+class Header extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <header>
+                header
+            </header>
+        );
+    }
+}
+
+export default Header;
